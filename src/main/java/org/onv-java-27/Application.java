8704b93128599dba35bda1e067e0ab134a0f6e40
ShibamD <>
2025-07-21 12:19:28 +0000

    Project init

---
+++ src/main/java/org/onv-java-27/Application.java
@@ -0,0 +1,13 @@
+package org.onv-java-27;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
